4ef19b7511688bbed07c676113b822e6364577a0
Sahil_JR <86585652+sahiljr-111@users.noreply.github.com>
2024-01-04 23:22:14 +0530

    modify OTP

--- controller/loginController.js
+++ controller/loginController.js
@@ -2,15 +2,64 @@ const userModel = require('../model/authModel')
 const bcrypt = require('bcrypt');
 var nodemailer = require('nodemailer');
 
-
+var userData = [];
+var OTP = ""
 exports.addClient = async (req, res) => {
   try {
     req.body.password = await bcrypt.hash(req.body.password, 10)
     const data = await userModel.create(req.body);
-    res.status(200).json({
-      status: "User added successfully!",
-      data
-    })
+    if (data) {
+      userData[0] = data;
+      var digits = '0123456789';
+      for (let i = 0; i < 4; i++) {
+        OTP += digits[Math.floor(Math.random() * 10)];
+      }
+      console.log(OTP)
+      var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'sahiljr1093@gmail.com',
+          pass: 'jbmoezfmthcilimv'
+        }
+      });
+      
+      var mailOptions = {
+        from: 'sahiljr1093@gmail.com',
+        to: req.body.email,
+        subject: 'OMNI-MARKET 🌐 Secure Code - Unleash the Delivering Now!',
+        text: `🚀 Welcome to OMNI-MARKET - Your Ultimate Diamond Companion!
+        Embark on a revolutionary Marketing experience with OMNI-MARKET! We're not just an app; we're your go- to marketplace for discovering the latest trends, unlocking exclusive deals, and indulging in a seamless shopping journey.
+      
+        🎁 Your One - Time Passcode(OTP) - Your Key to Savings!
+        To enhance your shopping security, we've sent you a unique OTP. Think of it as your golden ticket to unlock exclusive features and promotions within the app.
+      
+        🔓 Redeem Your OTP: ${OTP} 
+      
+        🚨 Note: Your OTP is confidential.Do not share it with anyone.
+
+        Open OMNI-MARKET.
+        Navigate to the login screen.
+        Enter your email and use the OTP to unlock a world of savings!
+      
+        Happy Marketing!
+        The OMNI-DEVELOPER Team 🛒`
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+      res.status(200).json({
+        status: "Enter OTP",
+      })
+    } else {
+      res.status(400).json({
+        status: 'data not found!'
+      })
+    }
   } catch (error) {
     res.status(500).json({
       error: error.toString()
@@ -18,58 +67,18 @@ exports.addClient = async (req, res) => {
   }
 }
 
-
-var userData = [];
-var OTP = ""
 exports.loginClient = async (req, res) => {
   try {
     if (!req.session.email) {
       const data = await userModel.find({ email: req.body.email });
-      console.log(data)
-      console.log(req.session.email)
       var solved = await bcrypt.compare(req.body.password, data[0].password);
       if (solved) {
         req.session.email = data[0].email;
         req.session.save();
+        console.log(req.session.email);
         res.status(200).json({
           data
         })
-        //   // OTP verification
-        //   userData[0] = data;
-        //   req.session.email = data[0].username;
-        //   req.session.save();
-        //   console.log(req.session.email)
-
-        //   var digits = '0123456789';
-        //   for (let i = 0; i < 4; i++) {
-        //     OTP += digits[Math.floor(Math.random() * 10)];
-        //   }
-        //   console.log(OTP)
-        //   var transporter = nodemailer.createTransport({
-        //     service: 'gmail',
-        //     auth: {
-        //       user: 'sahiljr1093@gmail.com',
-        //       pass: 'jbmoezfmthcilimv'
-        //     }
-        //   });
-
-        //   var mailOptions = {
-        //     from: 'sahiljr1093@gmail.com',
-        //     to: req.body.email,
-        //     subject: 'Omni-Market verification code *keepitsecret*',
-        //     text: OTP
-        //   };
-
-        //   transporter.sendMail(mailOptions, function (error, info) {
-        //     if (error) {
-        //       console.log(error);
-        //     } else {
-        //       console.log('Email sent: ' + info.response);
-        //     }
-        //   });
-        //   res.status(200).json({
-        //     status: "Enter OTP",
-        //   })
       } else {
         res.status(400).json({
           status: "Password is incorect!"
@@ -89,19 +98,16 @@ exports.loginClient = async (req, res) => {
 }
 
 exports.otpVerify = async (req, res) => {
-  // console.log(userData)
-  // if (req.body.otp == OTP) {
-  res.status(200).json({
-    userData
-  })
-  // } else {
-  //   req.session.destroy();
-  //   userData = ""
-  //   res.status(200).json({
-  //     status: "OTP incorrect"
-  //   })
-  // }
-  // OTP = "";
+  console.log(userData)
+  if (req.body.otp == OTP) {
+    res.status(200).json({
+      userData
+    })
+  } else {
+    res.status(200).json({
+      status: "OTP incorrect"
+    })
+  }
 }
 
 exports.logoutClient = async (req, res) => {
