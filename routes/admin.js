70603fa4fa0c0e7949cc4f75fb6d9f7f194c9594
Sahil_JR <86585652+sahiljr-111@users.noreply.github.com>
2024-03-05 17:32:56 +0530

    adding details of seller and buyer for admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { verifyToken } = require('../middleware/auth');
-const { loginAdmin, allSeller, allBuyer, allClient, chart } = require('../controller/adminController');
+const { loginAdmin, allSeller, allBuyer, allClient, chart, adminViewBids, adminViewPosts, sellerDetails, buyerDetails } = require('../controller/adminController');
 const { addDiamond, viewDiamond } = require('../controller/diamondController');
 
 router.post('/login', loginAdmin);
@@ -10,6 +10,13 @@ router.get('/allSeller', verifyToken, allSeller);
 router.get('/allBuyer', verifyToken, allBuyer);
 router.get('/allClient', verifyToken, allClient);
 router.get('/viewDiamond', verifyToken, viewDiamond);
+router.get('/viewBids', verifyToken, adminViewPosts);
+router.get('/viewPosts', verifyToken, adminViewBids);
 router.get('/chart', chart);
+
+
+//individual
+router.get('/seller-details/:id', verifyToken, sellerDetails);
+router.get('/buyer-details/:id', verifyToken, buyerDetails);
 
 module.exports = router;
