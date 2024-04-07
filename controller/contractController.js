7ac37d7cc9a93a3345229e3ce4243d4e361f8dc9
Sahil_JR <86585652+sahiljr-111@users.noreply.github.com>
2024-04-07 23:36:05 +0530

    give access to admin

--- controller/contractController.js
+++ controller/contractController.js
@@ -23,41 +23,34 @@ exports.addContract = async (req, res) => {
 
 exports.viewContract = async (req, res) => {
   try {
-    if (req.user.email) {
-      const data = await contractModel.find({
-        $or: [
-          { buyer_id: req.body.buyer_id },
-          { seller_id: req.body.seller_id }
-        ]
-      });
-      const postdata = await postsModel.findById({ _id: data[0].post_id })
-      if (postdata.isDeleted == true) {
-        res.status(200).json({
-          status: false,
-          msg: "post is deleted"
-        })
-      } else {
-        if (data != '') {
-          res.status(200).json({
-            status: true,
-            data
-          })
-        } else {
-          res.status(200).json({
-            status: false,
-            message: "Data not found"
-          })
-        }
-      }
-    } else {
+    const data = await contractModel.find({
+      $or: [
+        { buyer_id: req.body.buyer_id },
+        { seller_id: req.body.seller_id }
+      ]
+    });
+    const postdata = await postsModel.findById({ _id: data[0].post_id })
+    if (postdata.isDeleted == true) {
       res.status(200).json({
         status: false,
-        message: 'login first'
+        msg: "post is deleted"
       })
+    } else {
+      if (data != '') {
+        res.status(200).json({
+          status: true,
+          data
+        })
+      } else {
+        res.status(200).json({
+          status: false,
+          message: "Data not found"
+        })
+      }
     }
   } catch (error) {
-    res.status(500).json({
-      error: error.message
-    })
-  }
+  res.status(500).json({
+    error: error.message
+  })
+}
 }
